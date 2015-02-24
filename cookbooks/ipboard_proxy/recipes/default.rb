dd7c0dcf9a451df1ac10d4a25a4579243b213fdf
Moto Ohno <moto@bittorrent.com>
2015-02-24 14:56:37 -0800

    Mostly working, save for image/background-image links in css

--- cookbooks/ipboard_proxy/recipes/default.rb
+++ cookbooks/ipboard_proxy/recipes/default.rb
@@ -5,9 +5,27 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 include_recipe "apache2"
-%w(proxy proxy_ajp proxy_http rewrite deflate headers proxy_balancer
-	proxy_connect proxy_html).each do |apache_mod|
+%w(proxy proxy_ajp ssl proxy_http rewrite deflate headers proxy_balancer
+	proxy_connect xml2enc).each do |apache_mod|
 	include_recipe "apache2::mod_#{apache_mod}"
 end
 
-web_app "forum.getsync.com"
+apache_module 'proxy_html' do
+	conf true
+end
+
+%w(gd_bundle-g2-g1.crt star_getsync_com.crt star_getsync_com.key).each do |f|
+	cookbook_file f do
+		path "/etc/apache2/ssl/#{f}"
+		owner 'root'
+		group 'root'
+		mode 0600
+	end
+end
+
+web_app "forum.getsync.com" do
+	server_name "forum.getsync.com"
+	server_aliases [ node['hostname'] ]
+	template "forum.getsync.com.conf.erb"
+	docroot "/var/www/html"
+end
